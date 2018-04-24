cc931ef1138a0f5b2d4d8cddb9ab032fe990e4a1
hpark <hpark11@wellesley.edu>
2018-04-23 23:02:46 -0400

    minor

--- script.js
+++ script.js
@@ -6,32 +6,32 @@ function setup() {
 }
 
 function draw() {
-    blendMode(BLEND);
-    background(0, 5);
-
     noStroke();
+    blendMode(BLEND);
+    background(0);
+
     var h = hour();
     blendMode(NORMAL);
     drawHour(h);
 
-    blendMode(SOFT_LIGHT);
+    blendMode(EXCLUSION);
     var m = minute();
     drawMinute(m);
 
-    blendMode(SOFT_LIGHT);
+    blendMode(OVERLAY);
     var s = second();
     drawSecond(s);
 }
 
 function drawHour(currentHour) {
     var r = 255 * noise(t / 2 + 10);
-    var g = 255 * noise(t / 2) / 2;
+    var g = 255 * noise(t / 2) / 5;
     var b = 255 * noise(t / 2);
-    fill(r, g, b, [50]);
+    fill(r, g, b);
     beginShape();
     var xoff = 0;
     var hourHeight = windowHeight - (windowHeight / 24 * currentHour);
-    var heightDiff = 20;
+    var heightDiff = 10;
 
     // Iterate over horizontal pixels
     for (var x = 0; x <= width; x += 10) {
@@ -53,7 +53,7 @@ function drawMinute(currentMinute) {
     var r = 255 * noise(t - 10);
     var g = 255 * noise(t) / 3;
     var b = 255 * noise(t - 15);
-    fill(r, g, b, [50]);
+    fill(r, g, b);
     beginShape();
     var xoff = 0;
     var minuteHeight = windowHeight - (windowHeight / 60 * currentMinute);
@@ -64,32 +64,32 @@ function drawMinute(currentMinute) {
         // Set the vertex
         vertex(x, y);
         // Increment x dimension for noise
-        xoff += 0.01;
+        xoff += 0.03;
     }
     // increment y dimension for noise
     yoff += 0.01;
     vertex(width, height);
     vertex(0, height);
     endShape(CLOSE);
-    t = t + 0.001;
+    t = t + 0.002;
 }
 
 function drawSecond(currentSecond) {
     var r = 255 * noise(t - 20);
     var g = 255 * noise(t - 10) / 4;
     var b = 255 * noise(t - 35);
-    fill(r, g, b, [50]);
+    fill(r, g, b);
     beginShape();
     var xoff = 0;
     var secondHeight = windowHeight - (windowHeight / 60 * currentSecond);
-    var heightDiff = 20;
+    var heightDiff = 30;
     // Iterate over horizontal pixels
     for (var x = 0; x <= width; x += 10) {
         var y = map(noise(xoff, yoff), 0, 0.5, (secondHeight - heightDiff), (secondHeight + heightDiff));
         // Set the vertex
         vertex(x, y);
         // Increment x dimension for noise
-        xoff += 0.01;
+        xoff += 0.05;
     }
     // increment y dimension for noise
     yoff += 0.01;

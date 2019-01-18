2595f0ef7165eae9805acdb7ea4361502f4f4e95
Haley <haleypark.design@gmail.com>
2019-01-18 22:38:44 +0900

    fixed seconds method

--- clock1/script.js
+++ clock1/script.js
@@ -1,107 +1,57 @@
-var yoff = 0.0; // 2nd dimension of perlin noise
-var t = 0;
+var yoff = 0,
+    t = 0;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight)
 }
 
 function draw() {
-    noStroke();
-    blendMode(BLEND);
-    background(0);
-
-    var h = hour();
-    blendMode(NORMAL);
-    drawHour(h);
-
-    blendMode(EXCLUSION);
-    var m = minute();
-    drawMinute(m);
-
-    blendMode(DIFFERENCE);
-    var d = new Date();
-    var s = (d.getSeconds() + d.getMilliseconds() / 1000).toFixed(3);
-    drawSecond(s);
-
-    console.log(h + ", " + m + ", " + s);
+    noStroke(), blendMode(BLEND), background(0);
+    var e = hour();
+    blendMode(NORMAL), drawHour(e), blendMode(EXCLUSION);
+    var i = minute();
+    drawMinute(i), blendMode(DIFFERENCE);
+    var t = new Date,
+        n = (t.getSeconds() + t.getMilliseconds() / 1e3).toFixed(3);
+    drawSecond(n), console.log(e + ", " + i + ", " + n)
 }
 
-function drawHour(currentHour) {
-    var r = 255 * noise(t / 2 + 10);
-    var g = 255 * noise(t / 2) / 5;
-    var b = 255 * noise(t / 2);
-    fill(r, g, b);
-    beginShape();
-    var xoff = 0;
-    var hourHeight = windowHeight - (windowHeight / 24 * currentHour);
-    var heightDiff = 10;
-
-    // Iterate over horizontal pixels
-    for (var x = 0; x <= (width + 50); x += 10) {
-        var y = map(noise(xoff, yoff), 0, 0.5, (hourHeight - heightDiff), (hourHeight + heightDiff));
-        // Set the vertex
-        vertex(x, y);
-        // Increment x dimension for noise
-        xoff += 0.01;
+function drawHour(e) {
+    var i = 255 * noise(t / 2 + 10),
+        n = 255 * noise(t / 2) / 5,
+        o = 255 * noise(t / 2);
+    fill(i, n, o), beginShape();
+    for (var d = 0, r = windowHeight - windowHeight / 24 * e, a = 0; a <= width + 50; a += 10) {
+        var w = map(noise(d, yoff), 0, .5, r - 10, r + 10);
+        vertex(a, w), d += .01
     }
-    // increment y dimension for noise
-    yoff += 0.001;
-    vertex(width, height);
-    vertex(0, height);
-    endShape(CLOSE);
-    t += 0.001;
+    yoff += .001, vertex(width, height), vertex(0, height), endShape(CLOSE), t += .001
 }
 
-function drawMinute(currentMinute) {
-    var r = 255 * noise(t - 10);
-    var g = 255 * noise(t) / 3;
-    var b = 255 * noise(t - 15);
-    fill(r, g, b);
-    beginShape();
-    var xoff = 0;
-    var minuteHeight = windowHeight - (windowHeight / 60 * currentMinute);
-    var heightDiff = 20;
-    // Iterate over horizontal pixels
-    for (var x = 0; x <= (width + 50); x += 10) {
-        var y = map(noise(xoff, yoff), 0, 0.5, (minuteHeight - heightDiff), (minuteHeight + heightDiff));
-        // Set the vertex
-        vertex(x, y);
-        // Increment x dimension for noise
-        xoff += 0.03;
+function drawMinute(e) {
+    var i = 255 * noise(t - 10),
+        n = 255 * noise(t) / 3,
+        o = 255 * noise(t - 15);
+    fill(i, n, o), beginShape();
+    for (var d = 0, r = windowHeight - windowHeight / 60 * e, a = 0; a <= width + 50; a += 10) {
+        var w = map(noise(d, yoff), 0, .5, r - 20, r + 20);
+        vertex(a, w), d += .03
     }
-    // increment y dimension for noise
-    yoff += 0.01;
-    vertex(width, height);
-    vertex(0, height);
-    endShape(CLOSE);
-    t += 0.002;
+    yoff += .01, vertex(width, height), vertex(0, height), endShape(CLOSE), t += .002
 }
 
-function drawSecond(currentSecond) {
-    var r = 255 * noise(t - 20);
-    var g = 255 * noise(t - 10) / 4;
-    var b = 255 * noise(t - 35);
-    fill(r, g, b);
-    beginShape();
-    var xoff = 0;
-    var secondHeight = windowHeight - (windowHeight / 60 * currentSecond);
-    var heightDiff = 30;
-    // Iterate over horizontal pixels
-    for (var x = 0; x <= (width + 50); x += 10) {
-        var y = map(noise(xoff, yoff), 0, 0.5, (secondHeight - heightDiff), (secondHeight + heightDiff));
-        // Set the vertex
-        vertex(x, y);
-        // Increment x dimension for noise
-        xoff += 0.02;
+function drawSecond(e) {
+    var i = 255 * noise(t - 20),
+        n = 255 * noise(t - 10) / 4,
+        o = 255 * noise(t - 35);
+    fill(i, n, o), beginShape();
+    for (var d = 0, r = windowHeight - windowHeight / 60 * e, a = 0; a <= width + 50; a += 10) {
+        var w = map(noise(d, yoff), 0, .5, r - 30, r + 30);
+        vertex(a, w), d += .02
     }
-    // increment y dimension for noise
-    yoff += 0.01;
-    vertex(width, height);
-    vertex(0, height);
-    endShape(CLOSE);
-    t += 0.001;
+    yoff += .01, vertex(width, height), vertex(0, height), endShape(CLOSE), t += .001
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight)
 }
